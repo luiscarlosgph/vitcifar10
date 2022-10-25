8ec386b0024061327fef280cd73fb5d9d881d907
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-10-25 16:47:22 +0000

    Data loading seems to be working.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@brief Setup for the vit-cifar10 package.
+@author Luis C. Garcia Peraza Herrera (luiscarlos.gph@gmail.com).
+"""
+import setuptools
+import unittest
+
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='vit-cifar10',
+    version='0.0.1',
+    description='Python module to train and test on CIFAR-10.',
+    author='Luis C. Garcia-Peraza Herrera',
+    author_email='luiscarlos.gph@gmail.com',
+    license='MIT License',
+    url='https://github.com/luiscarlosgph/dentalseg',
+    packages=[
+        'vit-cifar10',
+    ],
+    package_dir={
+        'vit-cifar10': 'src',
+    },
+    install_requires = [
+        'numpy', 
+        'torch',
+        'torchvision',
+        'timm',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    test_suite = 'test',
+)
---
+++ src/train.py
@@ -0,0 +1,136 @@
+"""
+@brief  Main script to kick off the training.
+@author Luis Carlos Garcia Peraza Herrera (luiscarlos.gph@gmail.com).
+@date   25 Oct 2022.
+"""
+
+import argparse
+import torch
+import torchvision
+import timm
+import numpy as np
+
+# My imports
+
+# Fix random seeds for reproducibility
+seed = 0
+torch.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(seed)
+
+
+def help(short_option):
+    """
+    @returns The string with the help information for each command line option.
+    """
+    help_msg = {
+        '--lr' : 'Learning rate (required: True)',  # ResNets: 1e-3, ViT: 1e-4
+        '--opt': 'Optimizer (required: True)', 
+    }
+    return help_msg[short_option]
+
+
+def parse_cmdline_params():
+    """@returns The argparse args object."""
+    args = argparse.ArgumentParser(description='PyTorch ViT for training/testing on CIFAR-10.')
+    args.add_argument('--lr', required=True, type=float, help=help('--lr'))  
+    args.add_argument('--opt', required=True, type=str, help=help('--opt'))
+    #args.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    #args.add_argument('--noaug', action='store_true', help='disable use randomaug')
+    #args.add_argument('--noamp', action='store_true', help='disable mixed precision training. for older pytorch versions')
+    #args.add_argument('--nowandb', action='store_true', help='disable wandb')
+    #args.add_argument('--mixup', action='store_true', help='add mixup augumentations')
+    #args.add_argument('--net', default='vit')
+    #args.add_argument('--bs', default='512')
+    #args.add_argument('--size', default="32")
+    #args.add_argument('--n_epochs', type=int, default='200')
+    #args.add_argument('--patch', default='4', type=int, help="patch for ViT")
+    #args.add_argument('--dimhead', default="512", type=int)
+    #args.add_argument('--convkernel', default='8', type=int, help="parameter for convmixer")  
+
+    return  args.parse_args()
+
+
+def load_dataset(train_preproc_tf, test_preproc_tf, train_bs: int = 512, 
+        test_bs: int = 100, num_workers: int = 8):
+    """
+    @brief Function that creates the dataloaders of CIFAR-10.
+
+    @param[in]  train_bs  Training batch size.
+    @param[in]  test_bs   Testing batch size.
+
+    @returns a tuple with the training and testing dataloaders.
+    """
+    # Load training and testing sets
+    train_ds = torchvision.datasets.CIFAR10(root='./data', train=True, 
+                                            download=True, transform=train_preproc_tf)
+    test_ds = torchvision.datasets.CIFAR10(root='./data', train=False, 
+                                           download=True, transform=test_preproc_tf)
+
+    # Create dataloaders
+    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=train_bs, 
+                                           shuffle=True, num_workers=num_workers)
+    test_dl = torch.utils.data.DataLoader(test_ds, batch_size=test_bs, 
+                                          shuffle=False, num_workers=num_workers)
+
+    return train_dl, test_dl
+
+
+def build_preprocessing_transforms(size: int = 384):
+    """
+    @brief Preprocessing and data augmentation.
+
+    @param[in]  size  Target size of the images to be resized prior 
+                      processing by the network.
+
+    @returns a tuple of two transforms, one for training and another one for testing.
+    """
+    train_preproc_tf = torchvision.transforms.Compose([
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.Resize(size),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    test_preproc_tf = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(size),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    return train_preproc_tf, test_preproc_tf
+
+
+def build_model(nclasses: int = 10):
+    """
+    @brief Create Vision Transformer (ViT) model pre-trained on ImageNet-21k 
+           (14 million images, 21,843 classes) at resolution 224x224 
+           fine-tuned on ImageNet 2012 (1 million images, 1,000 classes) 
+           at resolution 384x384.
+
+    @param[in]  nclasses  Number of classes, CIFAR-10 has obviously 10 clsses.
+    """
+    net = timm.create_model('vit_base_patch16_384', pretrained=True)
+    net.head = torch.nn.Linear(net.head.in_features, nclasses)
+
+    return net
+
+
+def main():
+    # Parse command line parameters
+    args = parse_cmdline_params()
+
+    # Prepare preprocessing layers
+    train_preproc_tf, test_preproc_tf = build_preprocessing_transforms()
+
+    # Get dataloaders for training and testing
+    train_dl, test_dl = load_dataset(train_preproc_tf, test_preproc_tf)
+
+    # Build model
+    net = build_model()
+
+
+if __name__ == '__main__':
+    main()
