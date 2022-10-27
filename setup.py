a7bef8bd4627fcf418647a92f9fcf719b00e9dec
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2022-10-27 19:17:19 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='vitcifar10',
-    version='0.0.3',
+    version='0.0.4',
     description='Python module to train and test on CIFAR-10.',
     author='Luis C. Garcia-Peraza Herrera',
     author_email='luiscarlos.gph@gmail.com',
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 from .vitcifar10 import *
 from .randomaug import *
