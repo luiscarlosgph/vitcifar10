49ffacf7c9faed9eb686b5543a0ab72c0536e759
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2023-04-17 14:42:34 +0000

    Batch testing not working yet.

--- src/scripts/batch_testing.sh
+++ src/scripts/batch_testing.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # @brief  Script that launches the vitcifar10.test script over a set of 
 #         models (different random initialisations).
 # @author Luis C. Garcia Peraza Herrera (luiscarlos.gph@gmail.com).
@@ -7,5 +9,23 @@
 BATCH_SIZE=1
 PROGRAM="python3 -m vitcifar10.test"
 CIFAR10_DATA_DIR="./data"
+CHECKPOINT_DIR="./active_learning_2022/fully_supervised_without_pretraining/random/checkpoints"
 
-$PROGRAM --data $CIFAR_10_DATA_DIR --resume active_learning_2022/fully_supervised_without_pretraining/random/checkpoints/budget_100/iter_5/model_best.pt --bs $BATCH_SIZE
+# Create an array of budgets from the user input
+echo "Reading the list of budget iterations ... "
+IFS=',' 
+budgets=($2)
+
+# Create the array of iterations
+iterations=("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+for budget in "${budgets[@]}"
+do 
+    # Build test command
+    cmd="$PROGRAM --data $CIFAR10_DATA_DIR --resume $CHECKPOINT_DIR/budget_$budget/iter_$budget/model_best.pt --bs $BATCH_SIZE | grep 'Percentage of images correctly classified' | cut -d ' ' -f7 | cut -d '%' -f1"
+    
+    # Run inference
+    #acc=$($cmd)
+    print($cmd)
+done
+
